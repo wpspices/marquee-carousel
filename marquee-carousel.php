00ab1f8c0f9eb6869b05b8490c2b5b59b346f1a7
wpspices <wpspices@gmail.com>
2024-12-20 07:10:30 +0530

    fixed image height

--- marquee-carousel.php
+++ marquee-carousel.php
@@ -4,7 +4,7 @@
  * Description:       Create simple eye-catching, customizable carousels with lightweight block plugin
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.1
+ * Version:           0.1.2
  * Author:            wpspices
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Marquee_Carousel' ) ) {
 
 	class Marquee_Carousel {
 
-		public $version = '0.1.1';
+		public $version = '0.1.2';
 
 		// The instance of this class
 		private static $instance = null;
